bf8159b1b62a844b5cad8c6b1334596587735928
andreacisc <andreavelez6555@gmail.com>
2025-10-24 12:44:19 -0400

    fixed start menu background, connected scenes through buttons, created about game scene

---
+++ our-2d-game/Assets/start_menu/game_rules_scene.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"): # using "jump" b/c indicated by space
+		get_tree().change_scene_to_file("res://Assets/start_menu/start_scene.tscn")
---
+++ our-2d-game/start_scene.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$animated_planet.play()
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level 2/level_2_Mohammed.tscn")
+
+
+func _on_game_rules_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Assets/start_menu/game_rules_scene.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
